a6e510e74ef6de8e06903992c46a76730e79e74b
Reonu <danileon95@gmail.com>
2023-05-29 23:40:37 +0100

    Improved C2 performance

--- levels/wf/texscroll.inc.c
+++ levels/wf/texscroll.inc.c
@@ -1,12 +1,11 @@
-void scroll_wf_dl_slide_001_mesh_vtx_0() {
+void scroll_wf_dl_slide_001_mesh_layer_1_vtx_0() {
 	int i = 0;
 	int count = 174;
-	int width = 32 * 0x20;
 	int height = 32 * 0x20;
 
 	static int currentY = 0;
 	int deltaY;
-	Vtx *vertices = segmented_to_virtual(wf_dl_slide_001_mesh_vtx_0);
+	Vtx *vertices = segmented_to_virtual(wf_dl_slide_001_mesh_layer_1_vtx_0);
 
 	deltaY = (int)(2.0 * 0x20) % height;
 
@@ -20,15 +19,14 @@ void scroll_wf_dl_slide_001_mesh_vtx_0() {
 	currentY += deltaY;
 }
 
-void scroll_wf_dl_slide_002_mesh_vtx_0() {
+void scroll_wf_dl_slide_002_mesh_layer_1_vtx_0() {
 	int i = 0;
 	int count = 16;
-	int width = 32 * 0x20;
 	int height = 32 * 0x20;
 
 	static int currentY = 0;
 	int deltaY;
-	Vtx *vertices = segmented_to_virtual(wf_dl_slide_002_mesh_vtx_0);
+	Vtx *vertices = segmented_to_virtual(wf_dl_slide_002_mesh_layer_1_vtx_0);
 
 	deltaY = (int)(2.0 * 0x20) % height;
 
@@ -72,7 +70,7 @@ void scroll_wf_dl_Arena_mesh_layer_1_vtx_1() {
 }
 
 void scroll_wf() {
-	scroll_wf_dl_slide_001_mesh_vtx_0();
-	scroll_wf_dl_slide_002_mesh_vtx_0();
+	scroll_wf_dl_slide_001_mesh_layer_1_vtx_0();
+	scroll_wf_dl_slide_002_mesh_layer_1_vtx_0();
 	scroll_wf_dl_Arena_mesh_layer_1_vtx_1();
-}
+};
